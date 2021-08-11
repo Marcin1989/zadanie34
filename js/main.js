9040aabbc2791d7b0b3f62e4744d1e25bb835e44
marcin1989 <marcinlubanski23@gmail.com>
2021-08-11 09:19:14 +0200

    zadanie34

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let firstNumber = 7;
+let secondNumber = 9;
+
+function add(one,other) {
+    let suma = one + other;
+    return suma;
+}
+
+console.log(add(firstNumber,secondNumber)); 
+
+let wynik = add(firstNumber,secondNumber);
+
+function subtraction(one,other) {
+    let suma = one - other;
+    return suma;
+}
+ 
+console.log(subtraction(firstNumber,secondNumber))
+
+let resault = subtraction(firstNumber,secondNumber)
+
